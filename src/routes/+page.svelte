ef6f5a7d909bd533632a5954bb1a94038b195cfb
Chris Allen Moore <chris@chrisallenmoore.com>
2022-09-15 20:37:26 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <section class="page-px section-py">
 	<h1 class="text-3xl">Home</h1>
-	<h2 class="text-2xl">SvelteKit, Prisma, Auth Cookies</h2>
+	<h2 class="text-2xl">SvelteKit, Prisma, Auth with Cookies</h2>
 </section>
